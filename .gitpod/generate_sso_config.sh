8a00d2476391573b48a676f0bce69404a7ecbfc7
Vladimir Avdoshka <vova.avdoshka@gmail.com>
2021-11-03 18:44:39 +0000

    fixes

--- .gitpod/generate_sso_config.sh
+++ .gitpod/generate_sso_config.sh
@@ -1,10 +1,14 @@
+check_env_var() {
+    if [[ -z ${!1+set} ]]; then echo "$1 should be set as workspace environment variable. Ref: https://www.gitpod.io/docs/environment-variables"; fi
+}
+
 check_env_var AWS_SSO_START_URL
 check_env_var AWS_SSO_REGION
 check_env_var AWS_SSO_ACCOUNT_ID
 check_env_var AWS_SSO_ROLE_NAME
 check_env_var AWS_REGION
 
-mkdir ~/.aws3 && cat << EOF > ~/.aws3/config
+mkdir ~/.aws && cat << EOF > ~/.aws/config
 [default]
 sso_start_url = ${AWS_SSO_START_URL}
 sso_region = ${AWS_SSO_REGION}
@@ -12,7 +16,3 @@ sso_account_id = ${AWS_SSO_ACCOUNT_ID}
 sso_role_name = ${AWS_SSO_ROLE_NAME}
 region = ${AWS_REGION}
 EOF
-
-check_env_var() {
-    if [[ -z ${!1+set} ]]; then echo "$1 should be set as workspace environment variable. Ref: https://www.gitpod.io/docs/environment-variables"; fi
-}
